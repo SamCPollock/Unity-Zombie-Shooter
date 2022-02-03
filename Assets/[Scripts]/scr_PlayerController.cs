5636c662eb0c18d36fef62ed826f88361101b56b
Sam Pollock <samcpollock@gmail.com>
2022-02-02 19:37:41 -0500

    Added animations

---
+++ Assets/[Scripts]/scr_PlayerController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PlayerController : MonoBehaviour
+{
+    public bool isFiring;
+    public bool isReloading;
+    public bool isJumping;
+    public bool isRunning;
+
+
+}
